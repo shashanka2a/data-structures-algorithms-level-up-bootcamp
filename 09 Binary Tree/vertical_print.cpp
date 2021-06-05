c190badc7c4542c9de246ae058b5bed351515c30
shashanka2a <37143174+shashanka2a@users.noreply.github.com>
2021-06-05 14:27:17 +0530

    Update vertical_print.cpp

--- 09 Binary Tree/vertical_print.cpp
+++ 09 Binary Tree/vertical_print.cpp
@@ -1,121 +1,122 @@
-#include<iostream>
-#include<queue>
-#include<vector>
-#include<map>
+// https://www.hackerrank.com/contests/smart-interviews/challenges/si-vertical-order-of-tree/problem
+/*
+
+Given an array of unique elements, construct a Binary Search Tree and print the tree in a Vertical Order, starting from the left-most node. Print the nodes in each vertical in sorted order.
+
+Input Format
+
+First line of input contains T - number of test cases. Its followed by 2T lines. First line of each test case contains N - number of nodes in the BST. The next line contains N unique integers - value of the nodes.
+
+Constraints
+
+1 <= T <= 1000
+1 <= N <= 1000
+0 <= ar[i] <= 10000
+
+Output Format
+
+For each test case, print the Vertical Levels of the Binary Search Tree, separate each level by newline. Separate the output of different test cases with an extra newline.
+
+Sample Input 0
+
+3
+5
+1 2 3 4 5 
+5
+3 1 2 5 4 
+7
+4 5 15 0 1 7 17 
+
+Sample Output 0
+
+1 
+2 
+3 
+4 
+5 
+
+1 
+2 3 4 
+5 
+
+0 
+1 4 
+5 7 
+15 
+17
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
 using namespace std;
 
-class node{
-
-public:
-	int data;
-	node*left;
-	node*right;
-	
-	node(int d){
-		data = d;
-		left = NULL;
-		right = NULL;
-	}
+class Node {
+    public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d) {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
 };
 
-//Input : 1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
-node* buildTree(){
+Node* insert(Node* h,int k){
+    if(h==NULL) 
+        return new Node(k);
+    if(h->data<k) 
+        h->right= insert(h->right,k);
+    else 
+        h->left=insert(h->left,k);
+    return h;
+}  
 
-	int d;
-	cin>>d;
-
-	if(d==-1){
-		return NULL;
-	}
-
-	node* n = new node(d);
-	n->left = buildTree();
-	n->right = buildTree();
-
-	return n;
+void Helper(Node* root,int d,map<int,set<int>> &m){
+    if(root==NULL){
+       return; 
+    }
+    m[d].insert(root->data);
+    Helper(root->left,d-1,m);
+    Helper(root->right,d+1,m);
+    
 }
 
-void levelOrderPrint(node*root){
+// Vertical Level means distance from root node
+// Left side travesal is negative distance ( -1 ) 
+// Right side traversal postive distance ( +1 ) 
+// Store the level wise node values in a map of int,set<int>
+// InOrder Approach -> Building Solution in Top-Down Fashion
 
-	queue<node*> q;
-	q.push(root);
-	q.push(NULL);
-
-	while(!q.empty()){
-		node* temp = q.front();
-		if(temp==NULL){
-			cout<<endl;
-			q.pop();
-			//insert a new null for the next level
-			if(!q.empty()){
-				q.push(NULL);
-			}
-		}
-		else{
-			q.pop();
-			cout<<temp->data<<" ";
-
-			if(temp->left){
-				q.push(temp->left);
-			}
-			if(temp->right){
-				q.push(temp->right);
-			}
-		}
-
-	}
-	return;
-}
-//===============================
-//ToDo:  Complete this Function |
-//===============================
-
-//helper
-void traverseTree(node*root, int d, map<int,vector<int> > &m){
-	if(root==NULL){
-		return;
-	}
-
-	m[d].push_back(root->data);
-	traverseTree(root->left, d-1,m);
-	traverseTree(root->right,d+1,m);
+void printVerticalOrder(Node* root){
+    map<int,set<int>> m;
+    Helper(root,0,m);
+    for(auto p:m){
+        set<int> s = p.second;
+        for(auto i:s){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
 }
 
-void verticalOrderPrint(node* root){
-		
-	map<int, vector<int> > m;
-	int d = 0;
-
-	traverseTree(root, d, m);
-
-	//Keys are sorted (-2, -1,0,1,2,3.....)
-	for(auto p:m){
-		int key = p.first;
-		vector<int> line = p.second;
-
-		for(auto data : line){
-			cout << data <<" ";
-		}
-		cout <<endl;
-
-	}	
-	return;
-}
-
-int main(){
-	
-	node* root = new node(1);
-	root->left = new node(2);
-    root->right = new node(3);
-    root->left->left = new node(4);
-    root->left->right = new node(5);
-    root->right->left = new node(6);
-    root->right->right = new node(7);
-    root->right->left->right = new node(8);
-    root->right->right->right = new node(9);
-
-    verticalOrderPrint(root);
-
-
-	return 0;
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        int x;
+        cin>>n;
+        Node *root = NULL;
+        for(int i=0;i<n;i++){
+            cin>>x;
+            root = insert(root,x);
+        }
+        printVerticalOrder(root);
+        cout<<endl;
+    }
+    return 0;
 }
